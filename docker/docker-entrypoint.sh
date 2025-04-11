de9bcde493bafeb65e72bafc61eb76e627f90779
Volkan Kundurayapan <volkan.kundurayapan@gmail.com>
2025-04-11 18:33:54 +0300

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Ensure persistent directories exist and are writable
+mkdir -p /persistent/vector-db
+mkdir -p /persistent/uploads
+mkdir -p /persistent/sqlite
+chmod -R 777 /persistent
 {
   cd /app/server/ &&
     npx prisma generate --schema=./prisma/schema.prisma &&
